62d4083a715cb1a3190b54c3f2056f240f4a089a
Moshfiqur Rahman <studentemam@gmail.com>
2025-05-01 12:46:03 +0600

    feat(route): add task update, status update, and delete routes

--- src/routes/task.routes.ts
+++ src/routes/task.routes.ts
@@ -9,6 +9,12 @@ router.post("/", auth(USER_ROLE.user, USER_ROLE.admin), TaskControllers.createTa
 
 router.get("/", auth(USER_ROLE.user, USER_ROLE.admin), TaskControllers.getAllTasksBySpecificUserController);
 
-router.get("/:id", auth(USER_ROLE.user, USER_ROLE.admin), TaskControllers.getTaskByIdController)
+router.get("/:id", auth(USER_ROLE.user, USER_ROLE.admin), TaskControllers.getTaskByIdController);
+
+router.patch("/:id", auth(USER_ROLE.user, USER_ROLE.admin), TaskControllers.updateTaskByIdController);
+
+router.patch("/:id/status", auth(USER_ROLE.user, USER_ROLE.admin), TaskControllers.updateTaskStatusByIdController);
+
+router.delete("/:id", auth(USER_ROLE.user, USER_ROLE.admin), TaskControllers.deleteTaskByIdController)
 
 export const TaskRoutes = router;
