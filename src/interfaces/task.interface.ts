d3507a66adfe08bb228bbf9dbaaae31ea2199bb2
Moshfiqur Rahman <studentemam@gmail.com>
2025-05-01 10:41:23 +0600

    feat: add Task mongoose model

--- src/interfaces/task.interface.ts
+++ src/interfaces/task.interface.ts
@@ -2,7 +2,7 @@ export type TTask = {
     title: string;
     description: string;
     category: "artsAndCraft" | "nature" | "family" | "sport" | "friends" | "meditation";
-    status: "allTask" | "onGoing" | "pending" | "collaborativeTask" | "done";
+    status?: "allTask" | "onGoing" | "pending" | "collaborativeTask" | "done";
     endDate: string;
     createdBy?: string;
     isDeleted?: boolean;
---
+++ src/models/task.model.ts
@@ -0,0 +1,40 @@
+import { model, Schema } from "mongoose";
+import { TTask } from "../interfaces";
+
+const taskSchema = new Schema<TTask>({
+    title: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    description: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    category: {
+        type: String,
+        required: true,
+        enum: ["artsAndCraft", "nature", "family", "sport", "friends", "meditation"]
+    },
+    status: {
+        type: String,
+        enum: ["allTask", "onGoing", "pending", "collaborativeTask", "done"],
+        default: "pending"
+    },
+    endDate: {
+        type: String,
+        required: true,
+    },
+    isDeleted: {
+        type: Boolean,
+        default: false,
+    }
+},
+    {
+        timestamps: true,
+        versionKey: false
+    }
+);
+
+export const Task = model<TTask>("Task", taskSchema);
