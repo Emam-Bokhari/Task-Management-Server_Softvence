0b51e86739eb035e3a040235c4fcce0d31142b78
Moshfiqur Rahman <studentemam@gmail.com>
2025-05-01 13:33:09 +0600

    add validateRequest middleware

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import { AuthControllers } from '../controllers/auth.controller';
+import { AuthValidation } from '../validations/auth.validation';
+import { validateRequest } from '../middlewares/validateRequestSchema';
 
 const router = express.Router();
 
-router.post('/register', AuthControllers.registeredUserController);
+router.post('/register', AuthValidation.registerValidation, validateRequest, AuthControllers.registeredUserController);
 
-router.post('/login', AuthControllers.loginUserController);
+router.post('/login', AuthValidation.loginValidation, validateRequest, AuthControllers.loginUserController);
 
 export const AuthRoutes = router;
